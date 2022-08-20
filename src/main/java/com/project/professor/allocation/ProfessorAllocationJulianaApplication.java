451e7fcce8fcd99be9ba22035613a3973b16d4ae
JulianaC <juliana.karinacm@gmail.com>
2022-08-20 11:42:46 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationJulianaApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationJulianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationJulianaApplication.class, args);
+	}
+
+}
